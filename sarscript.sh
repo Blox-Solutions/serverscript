25cd5f6dbc10d2403a3d0f3a623c4cc1d62c311e
ih3na <ihenakaarachchi@proton.me>
2022-10-16 02:01:53 +0530

    edit sarscript

--- sarscript.sh
+++ sarscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script creates sample sar file outputs
-sudo sar -u 5 10 | tee cpu.log
-sudo sar -r 5 10 | tee mem.log
-sudo sar -b 5 10 | tee io.log
-sudo sar -F 5 10 | tee fs.log
+sudo sar -u 5 10 | tee logs/cpu.log
+sudo sar -r 5 10 | tee logs/mem.log
+sudo sar -b 5 10 | tee logs/io.log
+sudo sar -F 5 10 | tee logs/fs.log
