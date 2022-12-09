4377b0599b20961c4abec38f06fd0db989e75502
IH3NA <66982318+ih3na@users.noreply.github.com>
2022-12-09 23:34:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ cd ~/.local/share/Blox
 git clone https://github.com/Blox-Solutions/serverscript
 git clone https://github.com/Blox-Solutions/API
 
-cd ../API
+cd API
 npm install
 npm start &
 
 sleep 5s
 
-cd serverscript
+cd ../serverscript
 bash run.sh &
