c2c1d9ada9af792599b1a01f00bb1551f91a6390
ih3na <ihenakaarachchi@proton.me>
2022-12-10 00:09:43 +0530

    installer update

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 while :
 do
-  sar -u 1 10 | tee logs/cpu.log &
-  sar -r 1 10 | tee logs/mem.log &
-  sar -b 1 10 | tee logs/io.log &
-  sar -F 1 10 | tee logs/fs.log &
+  sar -u 1 10 | tee logs/cpu.log
+  sar -r 1 10 | tee logs/mem.log
+  sar -b 1 10 | tee logs/io.log
+  sar -F 1 10 | tee logs/fs.log
 
-  python3 main.py &
+  python3 main.py
   
   cp ./logs/stats.json ../API/stats.json
   sleep 1m
