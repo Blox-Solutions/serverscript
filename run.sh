3e477fa08683413b3242dbb32a676c8e8b97f891
IH3NA <66982318+ih3na@users.noreply.github.com>
2022-12-09 23:30:50 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,14 @@ pip install -r requirements.txt
 
 while :
 do
-  sar -u 1 10 | tee logs/cpu.log
-  sar -r 1 10 | tee logs/mem.log
-  sar -b 1 10 | tee logs/io.log
-  sar -F 1 10 | tee logs/fs.log
+  sar -u 1 10 | tee logs/cpu.log &
+  sar -r 1 10 | tee logs/mem.log &
+  sar -b 1 10 | tee logs/io.log &
+  sar -F 1 10 | tee logs/fs.log &
 
   python3 main.py &
   
+  cp ./logs/stats.json ../API/stats.json
   sleep 1m
 
 done
